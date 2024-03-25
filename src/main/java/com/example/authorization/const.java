c90dca3c5825b8e685905167275e9eba206b706e
Прудова Виктория Андреевна <10210806@college.local>
2024-03-25 16:05:53 +0300

    Initial commit

---
+++ src/main/java/com/example/authorization/const.java
@@ -0,0 +1,10 @@
+package com.example.authorization;
+
+public class const{
+    public static final String USER_TABLE = "users";
+    public static final String USER_ID = "id";
+    public static final String USER_NAME = "name";
+    public static final String USER_LOGIN = "login";
+    public static final String USER_PASSWORD = "password";
+
+        }
